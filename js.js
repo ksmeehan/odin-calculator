d436c84c10e7c7ef0ecc4530100cb4a2a3cf5fbe
KS Meehan <29366207+ksmeehan@users.noreply.github.com>
2025-11-22 09:53:43 +1030

    Add floating point functionality

--- js.js
+++ js.js
@@ -15,10 +15,54 @@ function t1() {
 
 let Calc = {
     left: null,
+    leftPoint: false,
     right: null,
+    rightPoint: false,
     operator: null,
     freezeScreen: false,
     showAnswer: false,
+    getMultiplyer: function (num) {
+        let decimalPlaces = (String(num).length - 1) - String(num).indexOf(".");
+        let m = "1";
+        for (let i = 0; i < decimalPlaces; i++) {
+            m += "0";
+        }
+        return Number(m);
+    },
+    sum: function (num1, num2) {
+        return this.pointSafeAddSubtract(num1, num2, (a,b)=>a+b)
+    },
+    subtract: function (num1, num2) {
+        if (String(num1).includes(".") || String(num2).includes(".")) {
+            return this.pointSafeAddSubtract(num1, num2, (a, b) => a - b);
+        }
+        else return a - b;
+    },
+    multiply: function (num1, num2) {
+        // if (String(num1).includes(".") || String(num2).includes(".")) {
+        //     return this.pointSafe(num1, num2, (a, b) => a * b);
+        // }
+        return num1 * num2;
+    },
+    divide: function (num1, num2) {
+        // if (String(num1).includes(".") || String(num2).includes(".")) {
+        //     return this.pointSafe(num1, num2, (a, b) => a / b);
+        // }
+        // else
+             return num1 / num2;
+    },
+    pointSafeAddSubtract: function (num1, num2, func) {
+        if(!(String(num1).includes(".")) && !(String(num2).includes("."))) return func(num1, num2);
+        let num1Multiplyer = this.getMultiplyer(num1);
+        let num2Multiplyer = this.getMultiplyer(num2);
+        let multiplyer = num1Multiplyer > num2Multiplyer ? num1Multiplyer : num2Multiplyer;
+
+        num1 = num1 * multiplyer;
+        num2 = num2 * multiplyer;
+        let ans = func(num1, num2);
+        ans = ans/multiplyer;
+        return ans;
+    },
     getBtnType: function (str) { //digit, equals, operator, clear, point
         if (str === "Clear") return "clear";
         if (str.length != 1) {
@@ -61,24 +105,40 @@ let Calc = {
         switch (this.operator) {
             case "+":
                 this.showAnswer = true;
-                return this.left + this.right;
+                return this.sum(this.left, this.right);
                 break;
             case "-":
                 this.showAnswer = true;
-                return this.left - this.right;
+                return this.subtract(this.left, this.right);
                 break;
             case "*":
                 this.showAnswer = true;
-                return this.left * this.right;
+                return this.multiply(this.left, this.right);
                 break;
             case "/":
                 if (this.left === 0) {
                     console.warn("attempted divide by zero.");
                     return false;
                 }
-                return this.left / this.right;
+                return this.divide(this.left, this.right);
         }
     },
+    placePoint() {
+        if (this.left === null) this.left = "0."
+        else if (this.left !== null && this.operator === null) {
+            if (String(this.left).includes(".")) { } // do nothing
+            else {
+                this.left = this.left + ".";
+            }
+        }
+        else if (this.operator !== null && this.right === null) this.right = "0.";
+        if (this.right !== null) {
+            if (String(this.right).includes(".")) { } // do nothing
+            else {
+                this.right = this.right + ".";
+            }
+        }
+    }
 }
 
 
@@ -92,10 +152,9 @@ document.querySelector("#calculator").addEventListener("click", (event) => {
     let screenNode = document.querySelector(".screen");
 
     let btnType = Calc.getBtnType(input);
-
     switch (btnType) {
         case "digit":
-            if(Calc.showAnswer === true) {
+            if (Calc.showAnswer === true) {
                 Calc.clear();
                 Calc.showAnswer = false;
             }
@@ -108,22 +167,21 @@ document.querySelector("#calculator").addEventListener("click", (event) => {
             screenNode.innerText = Calc.getEq();
             break;
         case "operator":
-            if(Calc.left === null){
+            if (Calc.left === null) {
                 //do nothing.
             }
-            else if(Calc.right === null){
-
-                Calc.placeOperator(input);
+            else if (Calc.right === null) {
                 Calc.showAnswer = false;
+                Calc.placeOperator(input);
                 screenNode.innerText = Calc.getEq();
 
             } else { //there is content on the right of the calculator, evaluate.
                 let ans = Calc.eval();
                 Calc.clear()
                 Calc.left = ans;
-                screenNode.innerText = Calc.getEq();     
+                screenNode.innerText = Calc.getEq();
                 Calc.showAnswer = true;
-           
+
             }
 
             break;
@@ -134,5 +192,9 @@ document.querySelector("#calculator").addEventListener("click", (event) => {
             screenNode.innerText = Calc.getEq();
             Calc.showAnswer = true;
             break;
+        case "point":
+            console.log("point btn pressed");
+            Calc.placePoint();
+            screenNode.innerText = Calc.getEq();
     }
 });
